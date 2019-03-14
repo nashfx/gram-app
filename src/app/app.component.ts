6ce914d7acd13155417bdf1f1af2d3bfc67e844c
Daniel Duque <danielvduque@gmail.com>
2019-03-14 17:34:03 -0300

    show all images in the grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AppService } from './app.service';
 })
 export class AppComponent {
   	title = 'gram-app';
-  	tiles: any = [];
+  	photo: any = [];
  	user: any;
 	pics: any;
 
@@ -18,16 +18,25 @@ export class AppComponent {
 
 
 	ngOnInit(){
-		this.tiles = [
-		    {text: 'One', cols: 1, rows: 5, color: 'lightblue'},
-		    {text: 'Two', cols: 1, rows: 2 	, color: 'lightgreen'},
-		    {text: 'Three', cols: 1, rows: 4, color: 'lightpink'},
-		    {text: 'Four', cols: 1, rows: 4, color: '#DDBDF1'},
-		    {text: 'Six', cols: 1, rows: 3, color: '#A3A1B4'},
-		    {text: 'Five', cols: 2, rows: 8, color: '#DF1A34'},
-		    {text: 'Seven', cols: 1, rows: 3, color: '#BCDF34'},
-		    {text: 'Eight', cols: 1, rows: 3, color: '#A3AFC4'},
-		    {text: 'Nine', cols: 2, rows: 4, color: '#DFC321'}
+		this.photo = [
+		    {cols: 1, rows: 5, color: 'lightblue'},
+		    {cols: 1, rows: 2, color: 'lightgreen'},
+		    {cols: 1, rows: 4, color: 'lightpink'},
+		    {cols: 1, rows: 4, color: '#DDBDF1'},
+		    {cols: 1, rows: 3, color: '#A3A1B4'},
+		    {cols: 2, rows: 8, color: '#DF1A34'},
+		    {cols: 1, rows: 3, color: '#BCDF34'},
+		    {cols: 1, rows: 3, color: '#A3AFC4'},
+		    {cols: 2, rows: 4, color: '#DFC321'},
+		    {cols: 1, rows: 5, color: 'lightblue'},
+		    {cols: 1, rows: 2, color: 'lightgreen'},
+		    {cols: 1, rows: 4, color: 'lightpink'},
+		    {cols: 1, rows: 4, color: '#DDBDF1'},
+		    {cols: 1, rows: 3, color: '#A3A1B4'},
+		    {cols: 2, rows: 8, color: '#DF1A34'},
+		    {cols: 1, rows: 3, color: '#BCDF34'},
+		    {cols: 1, rows: 3, color: '#A3AFC4'},
+		    {cols: 2, rows: 4, color: '#DFC321'}
 		];
 
 		this.getUser();
@@ -42,7 +51,8 @@ export class AppComponent {
 
 	getPics(){
 		this.appService.getPics().subscribe((data:any) => {
-			console.log(data.data);
+			this.pics = data.data;
+			console.log(this.pics);
 		});		
 	}
 
